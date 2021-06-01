e28a2531dbedeea85ea21bd02e8b9c431f7d49e5
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2021-06-01 12:42:53 +0900

    Create maxentitystask.php

---
+++ src/nexuscore/task/maxentitystask.php
@@ -0,0 +1,54 @@
+<?php
+
+    namespace nexuscore\task;
+
+
+    use pocketmine\plugin\Plugin;
+    use pocketmine\scheduler\Task;
+    use pocketmine\entity\object\ItemEntity;
+    use pocketmine\entity\Human;
+    use pocketmine\entity\projectile\Arrow;
+    use nexuscore\arrow\NewArrow;
+    use nexuscore\arrow\AntiGravityArrow;
+    use nexuscore\arrow\DoubleGravityArrow;
+    use nexuscore\nexuscore;
+
+    class maxentitystask extends Task
+    {
+        private $plugin;
+
+        public function __construct(Plugin $plugin)
+        {
+            $this->plugin = $plugin;
+            $this->sended = false;
+        }
+
+        public function onRun(int $currentTick)
+        {
+            $amount = 0;
+            /** @var Level $level */
+            foreach($this->plugin->getServer()->getLevels() as $level){
+	            foreach($level->getEntities() as $entity){
+		            if($entity instanceof ItemEntity || $entity instanceof Arrow || $entity instanceof NewArrow || $entity instanceof AntiGravityArrow || $entity instanceof DoubleGravityArrow){
+                        $amount = $amount + 1;
+                }
+	            }
+            }
+             if($amount > 199 && $this->sended === false){
+               $this->plugin->getServer()->broadcastTip("§3エンティティが200個以上存在します。250個を超えると全てのエンティティがキルされます。");
+               $this->sended = true;
+             }
+             if($amount > 249){
+               $this->plugin->getServer()->broadcastTip("§3エンティティが250個以上存在するため、全てのエンティティをkillします。");
+               $this->sended = false;
+               /** @var Level $level */
+               foreach($this->plugin->getServer()->getLevels() as $level){
+	                 foreach($level->getEntities() as $entity){
+		                   if($entity instanceof ItemEntity || $entity instanceof Arrow || $entity instanceof NewArrow || $entity instanceof AntiGravityArrow || $entity instanceof DoubleGravityArrow){
+			                     $entity->kill();
+                       }
+	                 }
+               }
+             }
+        }  
+ }
