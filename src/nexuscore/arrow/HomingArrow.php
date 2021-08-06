41f8f609923bcfec2c31248c3f8c762c1d2789fe
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2021-08-06 18:17:20 +0900

    Create HomingArrow.php

---
+++ src/nexuscore/arrow/HomingArrow.php
@@ -0,0 +1,71 @@
+
+<?php
+namespace nexuscore\arrow;
+use pocketmine\entity\projectile\Arrow;
+use pocketmine\entity\{
+	Entity,
+	Living
+};
+use pocketmine\level\Level;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\event\entity\ProjectileHitEntityEvent;
+use pocketmine\math\RayTraceResult;
+
+final class HomingArrow extends Arrow{
+  protected $gravity = 0;
+	protected $damage = 2.5;
+	protected $punchKnockback = 10.0;
+	private $shooter;
+
+	public function __construct(Level $level, CompoundTag $nbt, ?Entity $entity = null, bool $critical = false){
+		parent::__construct(
+			$level,
+			$nbt,
+			$entity,
+			$critical
+		);
+		if($entity === null) return;
+		$this->setMotion($entity->getDirectionVector()->normalize()->multiply(10)); //速度 この値を基本速度に掛け算してます
+		$this->shooter = $entity;
+	}
+
+	public function entityBaseTick(int $tick = 1):bool{
+ 	  $newTarget = $this->getWorld()->getNearestEntity($this->location, 8.0, Living::class);
+
+		if($newTarget instanceof Living){
+			$currentTarget = $newTarget;
+		}else{
+      $currentTarget = null;
+    }
+
+		if($currentTarget !== null){
+			$vector = $currentTarget->getPosition()->add(0, $currentTarget->getEyeHeight() / 2, 0)->subtractVector($this->location)->divide(8.0);
+
+			$distance = $vector->lengthSquared();
+		}
+		foreach($this->level->getEntities() as $entity){
+			if(
+				!$entity instanceof Living
+					or
+				$this->shooter === null
+					or
+				$this->shooter->getId() === $entity->getId() //当たり判定を大きくしすぎると打った人に当たるのでそれの防止
+					or
+				$this->distance($entity) > 10 //当たり判定
+					or
+				($bb = $entity->getBoundingBox()) === null //エンティティが当たり判定を持っているか
+			) continue;
+
+			$this->onHitEntity(
+				$entity,
+				new RayTraceResult(
+					$bb,
+					$this->getDirection(), //ここ間違ってるかも... ごめん...
+					$entity
+				)
+			);
+        break;
+		}
+		return parent::entityBaseTick($tick);
+	}
+}
