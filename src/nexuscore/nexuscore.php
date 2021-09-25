359536ce852a4b782e9f4d8f0f482fc75c58b48a
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2021-09-25 11:10:36 +0900

    createBaseNBT関数を実装

--- src/nexuscore/nexuscore.php
+++ src/nexuscore/nexuscore.php
@@ -57,6 +57,13 @@ use pocketmine\network\mcpe\protocol\DisconnectPacket;
 
 /* NBT */
 use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\DoubleTag;
+use pocketmine\nbt\tag\ListTag;
+use pocketmine\nbt\tag\FloatTag;
+
+/* math */
+use pocketmine\math\Vector3;
+
 
 /* nexuscore special arrows */
 use nexuscore\arrow\AntiGravityArrow;
@@ -192,15 +199,14 @@ class nexuscore extends PluginBase implements Listener
     {
         $entity = $event->getEntity();
         if (!$entity instanceof Player) return;
-        /*
-        $nbt = Entity::createBaseNBT(
-            $entity->add(0, $entity->getEyeHeight(), 0),
+        
+        $nbt = $this->createBaseNBT(
+            $entity->getLocation()->add(0, $entity->getEyeHeight(), 0),
             $entity->getDirectionVector(),
             ($entity->yaw > 180 ? 360 : 0) - $entity->yaw,
             -$entity->pitch
         );
-        */
-        $nbt = null;
+        
 
         $diff = $entity->getItemUseDuration();
         $p = $diff / 20;
@@ -227,7 +233,7 @@ class nexuscore extends PluginBase implements Listener
     
     }
 
-
+/*
   public function onShootBow(\pocketmine\event\entity\EntityShootBowEvent $event) : void{
     $entity = $event->getEntity();
     $bow = $event->getBow();
@@ -251,7 +257,7 @@ if($projectile->namedtag->offsetExists("TeleportBow")){
   }
 }
   }
-
+*/
   public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
     {
         switch (strtolower($command->getName())) {
@@ -716,4 +722,25 @@ public function onDamage(EntityDamageByEntityEvent $event)
         //if ($tag->offsetExists($this->getName())) if (!$tag->getInt($this->getName()) == 0) return false;
         return true;
     }
+    public function createBaseNBT(Vector3 $pos, ?Vector3 $motion = null, float $yaw = 0.0, float $pitch = 0.0) : CompoundTag{
+		return new CompoundTag("", [
+			new ListTag([
+				new DoubleTag("", $pos->x),
+				new DoubleTag("", $pos->y),
+				new DoubleTag("", $pos->z)
+            ],
+            "Pos"),
+			new ListTag([
+				new DoubleTag("", $motion !== null ? $motion->x : 0.0),
+				new DoubleTag("", $motion !== null ? $motion->y : 0.0),
+				new DoubleTag("", $motion !== null ? $motion->z : 0.0)
+            ],
+            "Motion"),
+			new ListTag([
+				new FloatTag("", $yaw),
+				new FloatTag("", $pitch)
+            ],
+            "Rotation")
+		]);
+	}
 }
