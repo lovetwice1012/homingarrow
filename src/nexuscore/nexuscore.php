b5b2bf271a97988faca8a8746b8bac5208742934
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2021-09-25 11:18:41 +0900

    DoubleTagとFloatTagの因数を更新していなかった

--- src/nexuscore/nexuscore.php
+++ src/nexuscore/nexuscore.php
@@ -725,20 +725,20 @@ public function onDamage(EntityDamageByEntityEvent $event)
     public function createBaseNBT(Vector3 $pos, ?Vector3 $motion = null, float $yaw = 0.0, float $pitch = 0.0) : CompoundTag{
 		return new CompoundTag("", [
 			new ListTag([
-				new DoubleTag("", $pos->x),
-				new DoubleTag("", $pos->y),
-				new DoubleTag("", $pos->z)
+				new DoubleTag($pos->x),
+				new DoubleTag($pos->y),
+				new DoubleTag($pos->z)
             ],
             "Pos"),
 			new ListTag([
-				new DoubleTag("", $motion !== null ? $motion->x : 0.0),
-				new DoubleTag("", $motion !== null ? $motion->y : 0.0),
-				new DoubleTag("", $motion !== null ? $motion->z : 0.0)
+				new DoubleTag($motion !== null ? $motion->x : 0.0),
+				new DoubleTag($motion !== null ? $motion->y : 0.0),
+				new DoubleTag($motion !== null ? $motion->z : 0.0)
             ],
             "Motion"),
 			new ListTag([
-				new FloatTag("", $yaw),
-				new FloatTag("", $pitch)
+				new FloatTag($yaw),
+				new FloatTag($pitch)
             ],
             "Rotation")
 		]);
